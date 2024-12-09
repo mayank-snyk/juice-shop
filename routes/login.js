da3af7c2572531e0e788d52c52323bca18bb111d
Mayank Khera <146714139+mayank-snyk@users.noreply.github.com>
2024-12-09 13:54:29 +0530

    removing vuln

--- routes/login.js
+++ routes/login.js
@@ -10,6 +10,7 @@ const challenges = require('../data/datacache').challenges
 const users = require('../data/datacache').users
 const config = require('config')
 
+/*
 // Vulnerable Code for SQL Injection
 const express = require('express');
 const router = express.Router();
@@ -35,7 +36,7 @@ router.post('/login', (req, res) => {
 });
 
 module.exports = router;
-
+*/
 
 
 module.exports = function login () {
